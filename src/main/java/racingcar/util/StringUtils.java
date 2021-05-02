47005c539d7b0b9373c8f19b437c4e83d10e66fb
HyungjuJang <dacapolife87@gmail.com>
2021-05-02 23:21:09 +0900

    feat : StringUtils 기능 추가 - 고정된 문자열 길이 변경 기능구현 - 콤마, 빈공백등 상수로 추출

--- src/main/java/racingcar/util/StringUtils.java
+++ src/main/java/racingcar/util/StringUtils.java
@@ -2,7 +2,22 @@ package racingcar.util;
 
 public class StringUtils {
 
+    private static final String SEPARATOR_COMMA = ",";
+    private static final String FIXED_FIVE_SPACE = "     ";
+
+    private StringUtils() {
+    }
+
     public static String[] separator(String input) {
-        return input.split(",");
+        return input.split(SEPARATOR_COMMA);
+    }
+
+    public static String fixedLengthString(String input) {
+        StringBuilder sb = new StringBuilder();
+
+        String newPrefix = FIXED_FIVE_SPACE.substring(input.length());
+        sb.append(input).append(newPrefix);
+
+        return sb.toString();
     }
 }
