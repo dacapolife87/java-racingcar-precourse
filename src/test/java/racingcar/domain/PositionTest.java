7b6f074b27f0744d432b24de1a69dbb64d285ba9
HyungjuJang <dacapolife87@gmail.com>
2021-05-03 00:23:16 +0900

    test : Position값의 동일한지 동일하지 않은지 테스트케이스 추가

--- src/test/java/racingcar/domain/PositionTest.java
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PositionTest {
 
@@ -24,6 +26,24 @@ class PositionTest {
 
         position.movePosition();
         assertThat(position.getPosition()).isEqualTo(lastPosition);
+    }
+
+    @DisplayName("Position객체의 값이 동일한지 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1", "2", "3"})
+    void samePositionTest(int positionNumber) {
+        Position position = Position.create(positionNumber);
+
+        assertTrue(position.samePosition(Position.create(positionNumber)));
+    }
+
+    @DisplayName("Position객체의 값이 동일하지 않은경우 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:2", "2:1", "3:4"}, delimiter = ':')
+    void notEqualsPositionTest(int positionNumber, int anotherPosition) {
+        Position position = Position.create(positionNumber);
+
+        assertFalse(position.samePosition(Position.create(anotherPosition)));
     }
 
 }
