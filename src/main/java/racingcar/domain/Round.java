bb33e8a239f57d5f6828d6000ce143ff88535b48
HyungjuJang <dacapolife87@gmail.com>
2021-05-02 23:20:46 +0900

    feat : Round관리 객체 구현

---
+++ src/main/java/racingcar/domain/Round.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public class Round {
+
+    private int countOfRound;
+
+    public Round(int countOfRound) {
+        this.countOfRound = countOfRound;
+    }
+
+    public int getRound() {
+        return countOfRound;
+    }
+}
