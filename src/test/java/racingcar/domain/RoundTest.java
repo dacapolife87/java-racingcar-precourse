bdebbc7c8eac97b2aa58791f624f45ab3f153fdc
HyungjuJang <dacapolife87@gmail.com>
2021-05-03 00:37:10 +0900

    test : round값이 1보다 작을 경우 예외 테스트

--- src/test/java/racingcar/domain/RoundTest.java
+++ src/test/java/racingcar/domain/RoundTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RoundTest {
 
@@ -14,5 +15,13 @@ class RoundTest {
         Round round = new Round(roundNumber);
 
         assertThat(round.getRound()).isEqualTo(roundNumber);
+    }
+
+    @DisplayName("Round값이 0이하 일때 예외 테스트")
+    @Test
+    void minimumRoundValueExceptionTest() {
+        assertThatThrownBy(
+                () -> new Round(0)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
