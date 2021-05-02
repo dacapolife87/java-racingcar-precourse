7ea67bb9de4e7f480128fd808b81bfb056467e6a
HyungjuJang <dacapolife87@gmail.com>
2021-05-03 00:36:48 +0900

    refactor : round값이 최소1이상일 경우만 입력받도록 예외추가

--- src/main/java/racingcar/domain/Round.java
+++ src/main/java/racingcar/domain/Round.java
@@ -2,10 +2,15 @@ package racingcar.domain;
 
 public class Round {
 
+    private static final int MINIMUM_ROUND = 1;
     private int countOfRound;
 
     public Round(int countOfRound) {
         this.countOfRound = countOfRound;
+
+        if (countOfRound < MINIMUM_ROUND) {
+            throw new IllegalArgumentException("라운드값은 최소 1 이상이여야합니다.");
+        }
     }
 
     public int getRound() {
